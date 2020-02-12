d82bac045778048f4eff6bc66a3405859e3a2cab
Olalekan <lekan.olapeju@yahoo.com>
2020-02-12 15:00:30 -0700

    Completed Exercise 4

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome!");
+            
+            static string input(){
+                Console.WriteLine("Enter your name!");
+                string userInput = Console.ReadLine();
+                return userInput;
+            }
+
+            static string parseSmileyBefore(string userInput){
+                return ":( " + userInput;
+            }
+
+            static string parseSmileyAfter(string userInput){
+                return userInput+ " :)";
+            }
+
+            static void outputValues(string userInput){
+                Console.WriteLine(userInput);
+            }
+
+            var fromUser = input();
+            var parseBefore = parseSmileyBefore(fromUser);
+            var parseAfter = parseSmileyAfter(parseBefore);
+            outputValues(parseAfter);
+            
+
+            
+        }
+    }
+}
+
+
