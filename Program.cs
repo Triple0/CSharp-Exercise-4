d4f8a44bb2729594e4834407f2e64569c14acada
Olalekan <lekan.olapeju@yahoo.com>
2020-02-12 15:08:55 -0700

    Methods put outside the main class

--- Program.cs
+++ Program.cs
@@ -7,32 +7,28 @@ namespace CSharp_Exercise_4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello and Welcome!");
-            
-            static string input(){
-                Console.WriteLine("Enter your name!");
-                string userInput = Console.ReadLine();
-                return userInput;
-            }
-
-            static string parseSmileyBefore(string userInput){
-                return ":( " + userInput;
-            }
-
-            static string parseSmileyAfter(string userInput){
-                return userInput+ " :)";
-            }
-
-            static void outputValues(string userInput){
-                Console.WriteLine(userInput);
-            }
-
             var fromUser = input();
             var parseBefore = parseSmileyBefore(fromUser);
             var parseAfter = parseSmileyAfter(parseBefore);
-            outputValues(parseAfter);
-            
+            outputValues(parseAfter);      
+        }
 
-            
+        static string input(){
+            Console.WriteLine("Enter your name!");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static string parseSmileyBefore(string userInput){
+            return ":( " + userInput;
+        }
+
+        static string parseSmileyAfter(string userInput){
+            return userInput+ " :)";
+        }
+
+        static void outputValues(string userInput){
+            Console.WriteLine(userInput);
         }
     }
 }
